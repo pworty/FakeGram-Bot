45acc2e3580f7e3f4f9b37741ae5025f9bb8b68a
pworty <wortexid@gmail.com>
2020-04-17 19:10:22 +0300

    -added the basic ability to configure screenshot parameters (without format checking)

--- main.py
+++ main.py
@@ -31,31 +31,41 @@ def stop(update, context):
 def createchat(update, context):
     update.message.reply_text('Alright, now configure settings and press \'Generate\' to get the '
                               'screenshot', reply_markup=createchat_markup)
+    return 'Change param'
+
+
+def change_param(update, context):
     return update.message.text
 
 
 def time(update, context):
     context.user_data['Time'] = update.message.text
+    return 'Change param'
 
 
 def battery(update, context):
     context.user_data['Battery'] = update.message.text
+    return 'Change param'
 
 
 def background(update, context):
     context.user_data['Background'] = update.message.text
+    return 'Change param'
 
 
 def name(update, context):
     context.user_data['Name'] = update.message.text
+    return 'Change param'
 
 
 def last_online(update, context):
     context.user_data['Last online'] = update.message.text
+    return 'Change param'
 
 
 def profile_pic(update, context):
     context.user_data['Profile pic'] = update.message.text
+    return 'Change param'
 
 
 def add_message(update, context):
@@ -82,12 +92,13 @@ def VPN(state):
 
 def main():
     REQUEST_KWARGS = {'proxy_url': 'socks5://' + PROXY}
-    updater = Updater(TOKEN, use_context=True, request_kwargs=REQUEST_KWARGS)
+    updater = Updater(TOKEN, use_context=True)
 
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('createchat', createchat)],
 
         states={
+            'Change param': [MessageHandler(Filters.text, change_param, pass_user_data=True)],
             'Time': [MessageHandler(Filters.text, time, pass_user_data=True)],
             'Battery': [MessageHandler(Filters.text, battery, pass_user_data=True)],
             'Background': [MessageHandler(Filters.text, background, pass_user_data=True)],
@@ -105,6 +116,7 @@ def main():
     dp.add_handler(CommandHandler('start', start))
     dp.add_handler(CommandHandler('help', start))
     dp.add_handler(CommandHandler('settings', settings))
+    dp.add_handler(conv_handler)
     updater.start_polling()
 
     updater.idle()
