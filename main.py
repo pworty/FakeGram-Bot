1e2715cce1485e069f3425daafa7de452565826b
pworty <wortexid@gmail.com>
2020-04-21 20:22:33 +0300

    added a method to form value updates added InlineKeyboard for last online seen, not working yet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from telegram.ext import Updater, MessageHandler, Filters
 from telegram.ext import ConversationHandler, CommandHandler
-from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telegram import ReplyKeyboardMarkup, InlineKeyboardButton, InlineKeyboardMarkup
 
 from bot_data import *
 
@@ -18,18 +18,19 @@ time_keyboard = [['1', '2', '3'],
                  ['4', '5', '6'],
                  ['7', '8', '9'],
                  [':', '0', ':']]
-last_seen_keyboard = [['Online'],
-                      ['Last seen recently'],
-                      ['Last seen M minutes ago'],
-                      ['Last seen H hours ago'],
-                      ['Last seen yesterday at hh:mm'],
-                      ['Last seen dd.mm.yy'],
-                      ['Last seen a long time ago']]
+last_seen_keyboard = [[InlineKeyboardButton('Online')],
+                      [InlineKeyboardButton('Last seen recently')],
+                      [InlineKeyboardButton('Last seen M minutes ago')],
+                      [InlineKeyboardButton('Last seen H hours ago')],
+                      [InlineKeyboardButton('Last seen yesterday at hh:mm')],
+                      [InlineKeyboardButton('Last seen dd.mm.yy')],
+                      [InlineKeyboardButton('Last seen a long time ago')]]
 
 menu_markup = ReplyKeyboardMarkup(menu_keyboard, one_time_keyboard=False)
 createchat_markup = ReplyKeyboardMarkup(createchat_keyboard, one_time_keyboard=True)
-keypad_markup = ReplyKeyboardMarkup(keypad, one_time_keyboard=True)
-time_markup = ReplyKeyboardMarkup(time_keyboard, one_time_keyboard=True)
+keypad_markup = ReplyKeyboardMarkup(keypad, one_time_keyboard=False)
+time_markup = ReplyKeyboardMarkup(time_keyboard, one_time_keyboard=False)
+last_seen_markup = InlineKeyboardMarkup.from_button(InlineKeyboardButton('Last seen a long time ago'))
 
 
 def start(update, context):
@@ -37,6 +38,7 @@ def start(update, context):
                               "with customization of multiple variables on your screen.\n\n"
                               "What I can do:\n"
                               "/createchat - access fake chat screenshot creation menu\n"
+                              "(Right now it resets your settings!)\n"
                               "/presets - use one of the presets to creat a fake chat\n"
                               "/settings - configure default time/background/battery and more",
                               reply_markup=menu_markup)
@@ -62,68 +64,102 @@ def change_param(update, context):
     print(context.user_data)
     message = update.message.text.split()[0]
     if message == 'Time':
-        update.message.reply_text('Enter time in hh:mm format', reply_markup=time_markup)
+        update.message.reply_text('Enter time in hh:mm format')
     elif message == 'Battery':
-        update.message.reply_text('Enter battery percentage from 1 to 100 (no % sign)',
-                                  reply_markup=keypad_markup)
+        update.message.reply_text('Enter battery percentage from 1 to 100 (no % sign)')
     elif message == 'Background':
         update.message.reply_text('')
     elif message == 'Name':
         update.message.reply_text('Enter the user\'s name')
-    elif message == 'Last online':
-        update.message.reply_text('Choose one type from options')
+    elif message == 'Last' and update.message.text.split()[1] == 'online':
+        update.message.reply_text('Choose one type from options', reply_markup=last_seen_markup)
     elif message == 'Profile pic':
-        update.message.reply_text('')
+        update.message.reply_text('Send a profile picture')
     return message
 
 
+def method_value_update(update, context, method_name, index):
+    old = context.user_data.get(method_name, 'None')
+    new = update.message.text + '%'
+    createchat_keyboard[index[0]][index[1]] = f'{method_name} - {new}'
+    update.message.reply_text(f'{method_name} updated! ({old} -> {new})',
+                              reply_markup=createchat_markup)
+    context.user_data[method_name] = new
+
+
 def time(update, context):
-    if update.message.text.isdigit() and 0 < int(update.message.text) <= 100:
-        old = context.user_data.get('Battery', 'None')
-        new = update.message.text + '%'
-        createchat_keyboard[0][1] = f'Battery - {new}'
-        update.message.reply_text(f'Battery updated! ({old} -> {new})',
-                                  reply_markup=createchat_markup)
-        context.user_data['Battery'] = new
+    method_name = 'Battery'
+    index = (0, 0)
+    message = update.message.text
+
+    hh, mm = message.split(':')
+    if hh.isdigit() and mm.isdigit() and 1 <= len(hh) <= 2 and len(mm) == 2:
+        method_value_update(update, context, method_name, index)
         return 'Change param'
     else:
         update.message.reply_text('Wrong format, try again')
-        return 'Battery'
+        return method_name
 
 
 def battery(update, context):
+    method_name = 'Battery'
+    index = (0, 1)
     message = update.message.text
+
     if message.isdigit() and 0 < int(message) <= 100:
-        old = context.user_data.get('Battery', 'None')
-        new = update.message.text + '%'
-        createchat_keyboard[0][1] = f'Battery - {new}'
-        update.message.reply_text(f'Battery updated! ({old} -> {new})',
-                                  reply_markup=createchat_markup)
-        context.user_data['Battery'] = new
+        method_value_update(update, context, method_name, index)
         return 'Change param'
     else:
         update.message.reply_text('Wrong format, try again')
-        return 'Battery'
+        return method_name
 
 
 def background(update, context):
-    context.user_data['Background'] = update.message.text
-    return 'Change param'
+    method_name = 'Background'
+    index = (0, 2)
+    message = update.message.text
+
+    if message:
+        pass
+        return 'Change param'
+    else:
+        update.message.reply_text('Wrong format, try again')
+        return method_name
 
 
 def name(update, context):
-    context.user_data['Name'] = update.message.text
+    method_name = 'Name'
+    index = (1, 0)
+    message = update.message.text
+
+    method_value_update(update, context, method_name, index)
     return 'Change param'
 
 
 def last_online(update, context):
-    context.user_data['Last online'] = update.message.text
-    return 'Change param'
+    method_name = 'Last online'
+    index = (1, 1)
+    message = update.message.text
+
+    if message:
+        pass
+        return 'Change param'
+    else:
+        update.message.reply_text('Wrong format, try again')
+        return method_name
 
 
 def profile_pic(update, context):
-    context.user_data['Profile pic'] = update.message.text
-    return 'Change param'
+    method_name = 'Profile pic'
+    index = (1, 2)
+    message = update.message.text
+
+    if message:
+        pass
+        return 'Change param'
+    else:
+        update.message.reply_text('Wrong format, try again')
+        return method_name
 
 
 def add_message(update, context):
